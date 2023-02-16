28289dcae7c362505138c82b6c62c39d14e7c34c
edmundsamina <edmundsamina@gmail.com>
2023-02-16 11:40:00 +0000

    nav links

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav-container">
+      <ul className="nav-links-list">
+        <li className="nav-link">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-link">
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li className="nav-link">
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li className="nav-link">
+          <Link to="/basket">Basket</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
