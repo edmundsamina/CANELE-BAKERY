45779295726b38bc4ec3d69dee168a236352f888
Kylie Smaldon <k_smaldon@outlook.com>
2023-02-16 14:27:34 +0000

    add CSS to nav bar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../../styles/navFooter.css';
-
+import logo from '../../assets/images/logo.png'
 
 function Nav() {
   return (
     <nav className="nav-container">
+    <img className="logo" src={logo} alt="logo" ></img>
       <ul className="nav-links-list">
         <li className="nav-link">
           <Link to="/">Home</Link>
