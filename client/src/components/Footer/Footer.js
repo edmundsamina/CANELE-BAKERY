e17c6892b44211b512dc7a4fa7567a035a89af77
Kylie Smaldon <k_smaldon@outlook.com>
2023-02-16 16:04:01 +0000

    add footer css

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,8 +2,29 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div className="footer-item">
+        <p className='footer-heading'>Find us here</p>
+        <p className='footer-email'>hello@matrecanelle.com</p>
+      </div>
+
+      <div className="footer-location footer-item">
+        <div className="footer-address">
+          <p>23 Albert St.</p>
+          <p>Paris</p>
+          <p>France</p>
+        </div>
+        <div className="footer-phone">
+          <p>123-456-7890</p>
+        </div>
+      </div>
+      <div className="socials footer-item">
+        <img></img>
+        <img></img>
+        <img></img>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
