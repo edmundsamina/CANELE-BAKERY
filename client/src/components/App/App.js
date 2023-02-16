a038b9bec63cb724004090eaa7aac1255bdbe526
Kylie Smaldon <k_smaldon@outlook.com>
2023-02-16 11:30:20 +0000

    set up routes

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
+
+import Nav from '../Nav/Nav';
+import Footer from '../Footer/Footer';
+import Home from '../../pages/Home';
+import Shop from '../../pages/Shop';
+import Contact from '../../pages/Contact';
+import Checkout from '../../pages/Checkout';
+import Basket from '../../pages/Basket';
+
 
 function App() {
   return (
     <div className="App">
-    
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
