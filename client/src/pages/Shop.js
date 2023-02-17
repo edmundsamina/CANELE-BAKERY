1597b1ae34eb724a6cc47ec416d1fb3d68c3ff16
edmundsamina <edmundsamina@gmail.com>
2023-02-17 15:44:10 +0000

    shop css started

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect }from "react";
+import "../styles/shop.css";
+import Card from "../components/Card/Card";
+
+
 
 function Shop() {
+ const [products, setProducts] = useState([])
+
+useEffect(()=>{
+  async function getProducts(){
+    const response = await fetch('https://projectcanelebakery.onrender.com/products')
+    const data = await response.json()
+    console.log(data)
+    setProducts(data.payload)
+  } getProducts()
+}, [])
+
   return (
-    <div>Shop</div>
-  )
+    <section className="shop-section-container">
+      <div className="filtering-button-container">
+        <button className="all-filter-button">ALL</button>
+        <button className="breads-filter-button">BREADS</button>
+        <button className="breads-filter-button">PASTRIES</button>
+        <button className="cakes-filter-button">CAKES</button>
+      </div>
+     <div className="shop-card-container">
+     {products.filter((one, index) => index % 3 === 0).map((one)=>{
+      return <Card img={one.image} title={one.name} price={one.price}/>
+     })}
+      </div>
+    
+    </section>
+  );
 }
 
-export default Shop
+export default Shop;
